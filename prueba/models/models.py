ef9b896017603058eb29f47b9bf2f71437cc6ce3
JeisonCabrera <jeisonandrescabreracalderon@gmail.com>
2023-05-28 00:14:56 -0500

    Creacion de muodulos de centro de costos y procesos, muestra en inventario

--- prueba/models/models.py
+++ prueba/models/models.py
@@ -1,11 +1,38 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
 
-class prueba(models.Model):
+class x_prueba(models.Model):
     _inherit = 'product.template'
 
-    x_value = fields.Integer()
+    x_value = fields.Integer() 
+    x_centro_costos_onetime = fields.One2many('centro.costos.onetime', 'x_clave', string='Centro de costos One Time')
+    
 
+class x_centro_costos_onetime(models.Model):
+    _name = 'centro.costos.onetime'
+    _description = 'centro.costos.onetime'
+
+    x_clave = fields.Many2one('product.template')
+    x_name = fields.Char('Nombre')
+    x_centro_costos = fields.Many2one('centro.costos', 'Centro de costos')
+    x_proces = fields.Many2one('procesos', 'Procesos')
+    x_valor = fields.Float('Valor')
+    
+    
+
+
+class x_centro_costos(models.Model):
+    _name = 'centro.costos'
+    _description = 'centro.costos'
+
+    x_centro = fields.Char('Centro de costos')
+
+class x_procesos(models.Model):
+    _name = 'procesos'
+    _description = 'procesos'
+    
+    x_procesos = fields.Char('Procesos')
+    
 #comentarios    
 # class prueba(models.Model):
 #     _name = 'prueba.prueba'
