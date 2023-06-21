0d6d2b7a98c91dd5fc83b7e7d9e1951137f789e0
JeisonCabrera <jeisonandrescabreracalderon@gmail.com>
2023-06-21 11:46:01 -0500

    Actualizacion de modulo compras_yeapdata, mejorado y funcional con todos los requerimientos

--- compras_yeapdata/models/custom_purchase_order.py
+++ compras_yeapdata/models/custom_purchase_order.py
@@ -4,8 +4,11 @@ from odoo import models, fields, api
 class compras_yeapdata(models.Model):
     _inherit = 'purchase.order'
 
-    #Contacto relacionado con proveedo
+    #Contacto relacionado con proveedor
     contacto_proveedor_id = fields.Many2one('res.partner', string='CONTACTO', domain="[('parent_id', '=', partner_id)]")
+    #Direccion de factura
+    contacto_factura_id = fields.Many2one('res.partner', string='FACTURACIÓN', domain="['&', ('parent_id', '=', company_id), ('type', '=', 'invoice')]")
+    Orden_compra_emergencia  = fields.Boolean('Orden de compra de emergencia')
 
     #Informacion General
 
@@ -31,12 +34,24 @@ class compras_yeapdata(models.Model):
         ('yeapdata', 'Yeapdata')
     ], string='TIPO DE ORDEN')
     nombre_proyecto = fields.Char(string='NOMBRE DEL PROYECTO')
-    fecha_finalizacion_contrato = fields.Date('FECHA DE FINALIZACIÓN DE CONTRATO', index=True, copy=False, readonly=False)
+    fecha_finalizacion_contrato = fields.Date('FECHA DE FINALIZACIÓN DE CONTRATO')
 
     #VALORES ONE TIME
     valore_onetime_ids = fields.One2many('valores.onetime', 'clave_id', string='VALORES ONE TIME')
-    centro_cotos_onetime = fields.Char('CENTRO DE COSTOS ONE TIME', index=True, copy=False, readonly=False)
-    valor_oc_onetime = fields.Float('VALOR OC')
+    # centro_cotos_onetime = fields.Char('CENTRO DE COSTOS ONE TIME', index=True, copy=False, readonly=False)
+    centro_cotos_onetime = fields.Char(compute='_compute_centro_costos_onetime', string='CENTRO DE COSTOS ONE TIME', store=True)
+    @api.depends('valore_onetime_ids.centro_costos_id.name')
+    def _compute_centro_costos_onetime(self):
+        for order in self:
+            order.centro_cotos_onetime = ', '.join(order.valore_onetime_ids.centro_costos_id.mapped('name'))
+
+    # valor_oc_onetime = fields.Float('VALOR OC')
+    valor_oc_onetime = fields.Float(compute='_compute_valor_oc_onetime', string='VALOR OC')
+    @api.depends('valore_onetime_ids.valor')
+    def _compute_valor_oc_onetime(self):
+        for order in self:
+            order.valor_oc_onetime = sum(order.valore_onetime_ids.mapped('valor'))
+
     moneda_onetime_id = fields.Many2one('res.currency', string='MONEDA')
     trm_onetime = fields.Float(string='TRM', default='1')
     valor_oc_pesos_onetime = fields.Float(compute='_compute_valor_oc_pesos_onetime', store=True, string='VALOR DE OC EN PESOS')
@@ -44,12 +59,21 @@ class compras_yeapdata(models.Model):
     def _compute_valor_oc_pesos_onetime(self):
         for record in self:
             record.valor_oc_pesos_onetime = record.valor_oc_onetime * record.trm_onetime
-
     
     #VALORES RECURRENTES
     valore_recurrentes_ids = fields.One2many('valores.recurrentes', 'clave_id', string='VALORES RECURRENTES')
-    centro_cotos_recurrente = fields.Char('CENTRO DE COSTOS RECURRENTES', index=True, copy=False, readonly=False)
-    valor_oc_recurrente = fields.Float('VALOR OC')
+    centro_cotos_recurrente = fields.Char(compute='_compute_centro_cotos_recurrente', string='CENTRO DE COSTOS RECURRENTES', store=True)
+    @api.depends('valore_recurrentes_ids.centro_costos_id.name')
+    def _compute_centro_cotos_recurrente(self):
+        for order in self:
+            order.centro_cotos_recurrente = ', '.join(order.valore_recurrentes_ids.centro_costos_id.mapped('name'))
+
+    valor_oc_recurrente = fields.Float(compute='_compute_valor_oc_recurrente', string='VALOR OC')
+    @api.depends('valore_recurrentes_ids.valor')
+    def _compute_valor_oc_recurrente(self):
+        for order in self:
+            order.valor_oc_recurrente = sum(order.valore_recurrentes_ids.mapped('valor'))
+
     moneda_recurrente_id = fields.Many2one('res.currency', string='MONEDA')
     trm_recurrente = fields.Float(string='TRM', default='1')
 
@@ -85,12 +109,13 @@ class compras_yeapdata(models.Model):
     
     #Tratamiento de datos
     tratamiento_datos_id = fields.Many2one('tratamiento.datos', string='TRATAMIENTO DE DATOS')
-    #Direccion de factura
-    contacto_factura_id = fields.Many2one('res.partner', string='FACTURACIÓN', domain="['&', ('parent_id', '=', company_id), ('type', '=', 'invoice')]")
+    
     # Resumen
     tiempo_related = fields.Integer('TIEMPO EN MESES', related='tiempo_meses_recurrente')
     centro_costos_ot_related = fields.Char('CENTRO DE COSTOS ONE TIME', related="centro_cotos_onetime")
     centro_costos_rec_related = fields.Char('CENTRO DE COSTOS RECURRENTES', related="centro_cotos_recurrente")
+    fecha_finalizacion_contrato_related = fields.Date('FECHA DE FINALIZACIÓN DE CONTRATO', related='fecha_finalizacion_contrato')
+
 
     valor_total_oc = fields.Char(compute='_compute_valor_total_oc', string='VALOR TOTAL OC')
     @api.depends('valor_oc_onetime', 'valor_oc_recurrente')
@@ -103,3 +128,12 @@ class compras_yeapdata(models.Model):
     def _compute_valor_total_oc_pesos(self):
         for record in self:
             record.valor_total_oc_pesos = record.valor_oc_pesos_onetime + record.valor_oc_pesos_recurrente
+
+    nombre_orden_compra = fields.Char(compute='_compute_nombre_orden_compra', string='nombre_orden_compra')
+    @api.depends('name', 'tipo_orden', 'partner_id', 'razon_social_cliente_id', 'nombre_proyecto', 'Orden_compra_emergencia')
+    def _compute_nombre_orden_compra(self):
+        for record in self:
+            if record.Orden_compra_emergencia == True:
+                record.nombre_orden_compra = 'OC-EMR-%s-%s-%s-%s-%s' %(record.name, record.tipo_orden[:4], record.partner_id.name[:7], record.razon_social_cliente_id.name[:10], record.nombre_proyecto[:15])
+            else: 
+                record.nombre_orden_compra = 'OC-%s-%s-%s-%s-%s' %(record.name, record.tipo_orden[:4], record.partner_id.name[:7], record.razon_social_cliente_id.name[:10], record.nombre_proyecto[:15])
